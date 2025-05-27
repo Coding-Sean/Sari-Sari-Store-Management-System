91c8f88ee95d023855672b545828d867e27ce8f6
rjay-avancena <rjayavancena7@gmail.com>
2025-05-27 13:19:03 +0800

    Refactor index.php to remove routing logic and redirect to Products view

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
 <?php
-// Basic router for demo purposes
-$controller = $_GET['controller'] ?? 'product';
-$action = $_GET['action'] ?? 'index';
-
-switch ($controller) {
-    case 'product':
-        require_once 'controllers/ProductController.php';
-        $c = new ProductController();
-        break;
-    case 'sales':
-        require_once 'controllers/SalesController.php';
-        $c = new SalesController();
-        break;
-    case 'report':
-        require_once 'controllers/ReportController.php';
-        $c = new ReportController();
-        break;
-    default:
-        die("Unknown controller");
-}
-if (!method_exists($c, $action)) die("Unknown action");
-$c->$action();
+header('Location: views/products/Products.php');
 ?>
